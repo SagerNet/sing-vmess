38cc72672c88f554a6e11194b316ac0a4b17c713
世界 <i@sekai.icu>
2025-05-03 11:33:10 +0800

    Update utls to v1.7.0

--- go.mod
+++ go.mod
@@ -3,16 +3,16 @@ module github.com/sagernet/sing-vmess
 go 1.20
 
 require (
-	github.com/gofrs/uuid/v5 v5.3.0
-	github.com/sagernet/reality v0.0.0-20230406110435-ee17307e7691
-	github.com/sagernet/sing v0.6.0
-	github.com/sagernet/utls v1.6.7
-	golang.org/x/crypto v0.32.0
+	github.com/gofrs/uuid/v5 v5.3.2
+	github.com/metacubex/utls v1.7.0-alpha.2
+	github.com/sagernet/sing v0.6.7
+	golang.org/x/crypto v0.33.0
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.6 // indirect
 	github.com/cloudflare/circl v1.3.7 // indirect
-	github.com/klauspost/compress v1.17.4 // indirect
-	golang.org/x/sys v0.29.0 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	golang.org/x/exp v0.0.0-20240904232852-e7e105dedf7e // indirect
+	golang.org/x/sys v0.30.0 // indirect
 )
--- vless/vision_utls.go
+++ vless/vision_utls.go
@@ -7,16 +7,20 @@ import (
 	"reflect"
 	"unsafe"
 
+	utls "github.com/metacubex/utls"
 	"github.com/sagernet/sing/common"
-	utls "github.com/sagernet/utls"
 )
 
 func init() {
 	tlsRegistry = append(tlsRegistry, func(conn net.Conn) (loaded bool, netConn net.Conn, reflectType reflect.Type, reflectPointer uintptr) {
-		tlsConn, loaded := common.Cast[*utls.UConn](conn)
-		if !loaded {
-			return
+		uConn, loaded := common.Cast[*utls.UConn](conn)
+		if loaded {
+			return true, uConn.NetConn(), reflect.TypeOf(uConn.Conn).Elem(), uintptr(unsafe.Pointer(uConn.Conn))
 		}
-		return true, tlsConn.NetConn(), reflect.TypeOf(tlsConn.Conn).Elem(), uintptr(unsafe.Pointer(tlsConn.Conn))
+		tlsConn, loaded := common.Cast[*utls.Conn](conn)
+		if loaded {
+			return true, tlsConn.NetConn(), reflect.TypeOf(tlsConn).Elem(), uintptr(unsafe.Pointer(tlsConn))
+		}
+		return
 	})
 }
