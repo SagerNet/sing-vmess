3a8da1e3baff96ba453501e0959863fec2c30977
wwqgtxx <wwqgtxx@gmail.com>
2025-08-19 23:04:11 +0800

    Using CastReader in vision tlsRegistry

--- vless/vision_utls.go
+++ vless/vision_utls.go
@@ -7,18 +7,18 @@ import (
 	"reflect"
 	"unsafe"
 
-	"github.com/sagernet/sing/common"
+	N "github.com/sagernet/sing/common/network"
 
 	utls "github.com/metacubex/utls"
 )
 
 func init() {
 	tlsRegistry = append(tlsRegistry, func(conn net.Conn) (loaded bool, netConn net.Conn, reflectType reflect.Type, reflectPointer uintptr) {
-		uConn, loaded := common.Cast[*utls.UConn](conn)
+		uConn, loaded := N.CastReader[*utls.UConn](conn)
 		if loaded {
 			return true, uConn.NetConn(), reflect.TypeOf(uConn.Conn).Elem(), uintptr(unsafe.Pointer(uConn.Conn))
 		}
-		tlsConn, loaded := common.Cast[*utls.Conn](conn)
+		tlsConn, loaded := N.CastReader[*utls.Conn](conn)
 		if loaded {
 			return true, tlsConn.NetConn(), reflect.TypeOf(tlsConn).Elem(), uintptr(unsafe.Pointer(tlsConn))
 		}
